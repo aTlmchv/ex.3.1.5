c19ca6ebee862ad7965d239b8a19eae51a197e27
aTlmchv <a.m.tolmachev@yandex.ru>
2022-12-25 23:45:20 +0300

    .

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/MyUserDetailService.java
@@ -0,0 +1,32 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+        }
+        return user;
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public User addUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        User getUser = userRepository.getById(user.getId());
+        if (!StringUtils.isEmpty(user.getPassword())) {
+            user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        } else {
+            user.setPassword(getUser.getPassword());
+        }
+        userRepository.save(user);
+    }
+
+
+    @Override
+    @Transactional
+    public void removeUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+
+    public User getUserById(int id) {
+        return userRepository.getById(id);
+    }
+}
